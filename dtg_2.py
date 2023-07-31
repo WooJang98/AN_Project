1bc5d184fe77950552b5bec00d64c077edca6dc1
WooJang98 <wjk3463@naver.com>
2023-07-31 16:01:11 +0900

    Update dtg_2.py
    기기상태 판단 함수 추가

--- dtg_2.py
+++ dtg_2.py
@@ -18,13 +18,23 @@ def avg(dt, dtnum, last):
         sum += dt[j][dtnum]
     return round(sum/(last+1),1)
 
+def dtg_status(dt, last):
+    count = 0
+    for j in range(last):
+        if dt[j][11] == 11 and dt[j+1][11] == 11:
+            count += 1
+            if count>=900:
+                return 11
+        else:
+            count = 0
+    return 0
+
 plt.rc("font", family="Malgun Gothic")
 plt.rc("axes", unicode_minus=False)
 
 import koreanize_matplotlib
 
 df = pd.read_csv("C:/DTG_01.txt") # 지정한 경로에서 DTG파일 가져오기
-df.shape
 
 ts = df["트립시작일"].unique() #고유한 트립시작일 값을 넣은 trip_start 배열 선언
 tlen = len(ts) # 고유한 트립시작일 값의 개수
@@ -41,7 +51,7 @@ for i in range(tlen):
 
     tt[i][2] = dt[0][22] #운전자명, 운전자 코드 값
 
-    tt[i][3] = dt[0][11] #DTG상태, 기기상태값
+    tt[i][3] = dtg_status(dt, last) #DTG상태, 기기상태값
 
     tt[i][4] = int((datetime.strptime(dt[last][3],"%Y-%m-%d %H:%M:%S") - datetime.strptime(dt[0][2],"%Y-%m-%d %H:%M:%S")).total_seconds())
     #운행시간, (도착일시-트립시작일) 계산, 초단위로 변환 후 int 형으로 변환
@@ -73,6 +83,6 @@ for i in range(tlen):
 
 
 col_name = ['출발일시', '도착일시', '운전자명', 'DTG 상태', '운행시간', '운행거리', '최고속도', '평균속도', '최고RPM', '평균RPM', '최소전압', '최대전압','과속시간', '누적주행거리']
-tt = pd.DataFrame(tt, columns = col_name)
-tt.to_csv("dtg_01")
+#tt = pd.DataFrame(tt, columns = col_name)
+#tt.to_csv("dtg_01")
 
